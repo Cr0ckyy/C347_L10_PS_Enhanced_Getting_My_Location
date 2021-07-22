c68f9793e696227a319d3980f6391f0fdf794cf1
LI SHUFANG <lishufang1121@gmail.com>
2021-07-22 14:42:56 +0800

    Initial Commit

---
+++ app/src/main/java/com/myapplicationdev/c347_l10_psenhancedgettingmylocation/MainActivity.java
@@ -0,0 +1,14 @@
+package com.myapplicationdev.c347_l10_psenhancedgettingmylocation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
